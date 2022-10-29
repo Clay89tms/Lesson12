287bb63e5d2747b8afa49d72eb345687540dad5e
Clay89tms <bboyClay89@gmail.com>
2022-10-29 15:28:54 +0300

    ДЗ 12 [29.10.2022] (100%) Сделал отдельно v.2

---
+++ src/ScannerIn.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class ScannerIn {
+
+    public static String scanIn() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Укажите путь к созданию файлов:");
+        return scanner.nextLine();
+    }
+}
+
